2d39635b494f3bf614a6da987fbca6fdc22019e4
gburgett <gordon@gordonburgett.net>
2016-12-17 00:22:34 +0100

    dev server now processes and serves tap input

--- www/dev_server.ts
+++ www/dev_server.ts
@@ -1,7 +1,12 @@
 import { Application, Router, Request, Response, NextFunction } from 'express'
 import * as webpack from 'webpack'
+import * as path from 'path'
+import * as fs from 'fs'
+import { Transform } from 'stream'
 
 import { Server } from './_testserver'
+import { NodeTapParser } from '../src/parser'
+import { Summary, Test } from '../src/parser/results'
 
 var WebpackDevServer = require("webpack-dev-server");
 
@@ -17,18 +22,26 @@ console.log('entry: ', config.entry)
 config.plugins.push(new webpack.HotModuleReplacementPlugin())
 
 //start compiler and server
-var testApi: Server
+const testApi = new Server()
 var compiler = webpack(config)
 var server = new WebpackDevServer(compiler, {
   hot: true,
   setup: function(app: Application) {
     //setup test API
-    testApi = new Server(app)
+    testApi.init(app)
   }
 })
 
-if (!testApi) {
-  throw new Error('no test api')
-}
-
 server.listen(8080, "localhost", function() { })
+
+process.argv.forEach(function(val) {
+  // find and process the file
+  if (path.extname(val) === '.tap') {
+    fs.createReadStream(val)
+      .pipe(new NodeTapParser())
+      .pipe(testApi.writeableSource('file: ' + val))
+  }
+})
+
+process.stdin.pipe(new NodeTapParser())
+  .pipe(testApi.writeableSource('stdin'))
