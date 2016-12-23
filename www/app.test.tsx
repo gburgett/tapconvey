4a9d401bd310799a2fcdb9bec8f21c93f9aeec79
gburgett <gordon@gordonburgett.net>
2016-12-23 15:05:05 +0100

    make App get stdin test run and render TestRunView

--- www/app.test.tsx
+++ www/app.test.tsx
@@ -2,8 +2,12 @@ import 'mocha'
 import * as React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
+import { asMap } from 'mobx'
 
 import { App } from './app'
+import { TestRunList } from './TestRunList'
+import { TestRun } from './client'
+import { Summary } from '../src/parser/results'
 
 var jsdom = require('mocha-jsdom')
 
@@ -11,9 +15,29 @@ describe('<App />', () => {
 
   jsdom()
 
-  it('renders the app message', () => {
-    const app = shallow(<App />)
+  describe('single run from stdin', () => {
+    const run = new TestRun()
+    run.summary = new Summary()
+    run.summary.version = 13
+    run.summary.time = '245ms'
 
-    expect(app.find('h1').text()).to.equal('This is the app!')
+    it('renders the header message', () => {
+      const mock = new TestRunList(undefined)
+      const app = shallow(<App runs={mock} />)
+
+      expect(app.find('h2').text()).to.equal('Tapconvey')
+    })
+
+    it('renders a TestRunView with the test run', () => {
+      const mock = new TestRunList(undefined, asMap<TestRun>({
+        'stdin': run
+      }))
+      const app = shallow(<App runs={mock} />)
+
+      const view = app.find('TestRunView')
+      expect(view.prop('run')).to.equal(run)
+      expect(view.prop('source')).to.equal('stdin')
+      expect(view.key()).to.equal('stdin')
+    })
   })
 })
--- www/app.tsx
+++ www/app.tsx
@@ -1,11 +1,41 @@
 import * as React from 'react'
 
-export interface AppProps {
+import { TestRunList } from './TestRunList'
+import { TestRun } from './client'
 
+import { TestRunView } from './TestRunView'
+
+export interface AppProps {
+  runs: TestRunList
 }
 
 export class App extends React.Component<AppProps, undefined> {
   render() {
-    return <h1>This is the app!</h1>;
+    var testRuns
+
+    const stdin = this.props.runs.stdin
+    if (stdin && stdin.run) {
+      testRuns = this.renderTestRun(stdin.source, stdin.run)
+    } else {
+      testRuns = <h3>Runs from other sources not implemented yet</h3>
+    }
+
+    return (
+      <div>
+        <div>
+          <h2>Tapconvey</h2>
+        </div>
+        <div>
+          {testRuns}
+        </div>
+      </div>
+    )
+  }
+
+  renderTestRun(source: string, run: TestRun) {
+
+    return (
+      <TestRunView source={source} run={run} key={source} />
+    )
   }
 }
